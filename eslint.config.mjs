b6c2fd7de25f18f1a93c0f550491f1d009880ee5
ngthehoainam <nthnam.dev@email.com>
2025-12-12 01:24:48 +0700

    chore: update project configuration with linting and formatting tools

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import eslintConfigPrettier from 'eslint-config-prettier'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {},
+}).append(eslintConfigPrettier)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/eslint",
+    "@pinia/colada-nuxt",
+    "@ant-design-vue/nuxt",
+  ],
+  eslint: {
+    config: {
+      stylistic: false,
+    },
+  },
+});
